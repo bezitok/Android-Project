d2984b3a46695316f961a1e8e64647568734b004
bezitok <nguyenhaikiller@gmail.com>
2019-05-31 14:32:20 +0700

    nothing

---
+++ Assets/app/src/main/java/com/example/administrator/assets/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.assets;
+
+import android.content.res.AssetManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
